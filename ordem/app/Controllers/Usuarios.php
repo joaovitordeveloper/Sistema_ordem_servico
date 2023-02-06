fd6cfd927797ae67b1940fbdadd2745984206ff9
joao vitor <developer.joaovitor@gmail.com>
2023-02-06 19:50:58 -0300

    ajax da aplicação

--- ordem/app/Controllers/Usuarios.php
+++ ordem/app/Controllers/Usuarios.php
@@ -23,4 +23,35 @@ class Usuarios extends BaseController
 
         return \view('Usuarios/index', $data);
     }
+
+    public function recuperaUsuarios()
+    {
+        //valida se e uma requisição via ajax
+        if (!$this->request->isAJAX()) {
+            return \redirect()->back();
+        }
+
+        $atributos = [
+            'id',
+            'nome',
+            'email',
+            'ativo',
+            'imagem'
+        ];
+
+        $usuarios = $this->usuarioModel->select($atributos)->findAll();
+        //recebera o array de objetos para retornar conforme a documentação do dataTable
+        $data = [];
+        
+        foreach ($usuarios as $usuario) {
+           $data[] = [
+                'imagem' => $usuario->imagem,
+                'nome'   => esc($usuario->nome),//função esc para validar caracteres especiais propria do CI4
+                'email'  => esc($usuario->email),
+                'ativo'  => ($usuario->ativo == true ? 'Ativo' : '<span class = "text-warning">Inativo</span>')
+           ]; 
+        }
+
+        return $this->response->setJSON(['data' => $data]);
+    }
 }
--- ordem/app/Views/Usuarios/index.php
+++ ordem/app/Views/Usuarios/index.php
@@ -10,6 +10,7 @@
 <!-- Estilos da pagina -->
 <?php $this->section('estilos'); ?>
 
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.13.2/r-2.4.0/sl-1.6.0/datatables.min.css" />
 
 
 <?php $this->endSection(); ?>
@@ -18,55 +19,22 @@
 <?php $this->section('conteudo'); ?>
 
 <div class="row">
-    <div class="col-lg-12">
-      <div class="block">
-        <div class="title"><strong>Compact Table</strong></div>
-        <div class="table-responsive">
-          <table class="table table-striped table-sm">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter </td>
-              </tr>
-              <tr>
-                <th scope="row">4</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">5</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+  <div class="col-lg-12">
+    <div class="block">
+      <div class="table-responsive">
+        <table id="ajaxTable" class="table table-striped table-sm" style="width: 100%;">
+          <thead>
+            <tr>
+              <th>Imagem</th>
+              <th>Nome</th>
+              <th>E-mail</th>
+              <th>Situação</th>
+            </tr>
+          </thead>
+        </table>
       </div>
     </div>
+  </div>
 </div>
 
 <?php $this->endSection(); ?>
@@ -74,6 +42,60 @@
 <!-- Scripts da pagina -->
 <?php $this->section('scripts'); ?>
 
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.13.2/r-2.4.0/sl-1.6.0/datatables.min.js"></script>
+<<script>
+    $(document).ready(function () {
 
+      const DATATABLE_PTBR = {
+            "sEmptyTable": "Nenhum registro encontrado",
+            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "_MENU_ resultados por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            },
+            "select": {
+                "rows": {
+                    "_": "Selecionado %d linhas",
+                    "0": "Nenhuma linha selecionada",
+                    "1": "Selecionado 1 linha"
+                }
+            }
+        }
 
-<?php $this->endSection(); ?>
+      $('#ajaxTable').DataTable({
+        "oLanguage": DATATABLE_PTBR,
+
+        ajax: "<?php echo site_url('usuarios/recuperaUsuarios'); ?>",
+        columns: [
+        { data: 'imagem' },
+        { data: 'nome' },
+        { data: 'email' },
+        { data: 'ativo' },
+        ],
+        deferRender: true,
+        processing: true,
+        language: {
+          processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>',
+        },
+        responsive: true,
+        pagingType: $(window).width() < 768 ? "simple" : "simple_numbers",
+      });
+    });
+  </script>
+
+  <?php $this->endSection(); ?>
