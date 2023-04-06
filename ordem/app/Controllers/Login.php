aecbf44541b1c6edb4b94bc6f2e4b66489da4531
Joao Vitor <developer.joaovitor@gmail.com>
2023-04-06 07:46:24 -0300

    validação de login

--- ordem/app/Controllers/Login.php
+++ ordem/app/Controllers/Login.php
@@ -14,4 +14,35 @@ class Login extends BaseController
 
         return view('Login/novo', $data);
     }
+
+    public function logar()
+    {
+        if (!$this->request->isAJAX()) {
+            return redirect()->back();
+        }
+
+        $retorno['token'] = csrf_hash();
+
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+
+        $autenticacao = service('autenticacao');
+
+        if ($autenticacao->login($email, $password) === false) {
+            $retorno['erro'] = 'Por favor verifique os erros abaixo e tente novamente.';
+            $retorno['erros_model'] = ['credenciais' => 'Não encontramos suas credenciais de acesso.'];
+            return $this->response->setJSON($retorno);
+        }
+
+        $usuarioLogado = $autenticacao->pegaUsuarioLogado();
+        session()->setFlashdata('sucesso', "Olá $usuarioLogado->nome, que bom que esta de volta!");
+
+        if ($usuarioLogado->is_cliente) {
+            $retorno['redirect'] = 'ordens/minhas';
+            return $this->response->setJSON($retorno);
+        }
+
+        $retorno['redirect'] = 'home';
+        return $this->response->setJSON($retorno);
+    }
 }
--- ordem/app/Views/Login/novo.php
+++ ordem/app/Views/Login/novo.php
@@ -1,81 +1,138 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ordem de Serviço | <?php echo $titulo;?> </title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-    <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>vendor/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>vendor/font-awesome/css/font-awesome.min.css">
-    <!-- Custom Font Icons CSS-->
-    <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>css/font.css">
-    <!-- Google fonts - Muli-->
-    <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>https://fonts.googleapis.com/css?family=Muli:300,400,700">
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>css/custom.css">
-    <!-- Favicon-->
-    <link rel="shortcut icon" href="<?php echo site_url('recursos/') ?>img/favicon.ico">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Ordem de Serviço | <?php echo $titulo; ?> </title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="all,follow">
+  <!-- Bootstrap CSS-->
+  <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>vendor/bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome CSS-->
+  <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>vendor/font-awesome/css/font-awesome.min.css">
+  <!-- Custom Font Icons CSS-->
+  <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>css/font.css">
+  <!-- Google fonts - Muli-->
+  <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>https://fonts.googleapis.com/css?family=Muli:300,400,700">
+  <!-- theme stylesheet-->
+  <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>css/style.default.css" id="theme-stylesheet">
+  <!-- Custom stylesheet - for your changes-->
+  <link rel="stylesheet" href="<?php echo site_url('recursos/') ?>css/custom.css">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="<?php echo site_url('recursos/') ?>img/favicon.ico">
+  <!-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-  </head>
-  <body>
-    <div class="login-page">
-      <div class="container d-flex align-items-center">
-        <div class="form-holder has-shadow">
-          <div class="row">
-            <!-- Logo & Information Panel-->
-            <div class="col-lg-6">
-              <div class="info d-flex align-items-center">
-                <div class="content">
-                  <div class="logo">
-                    <h1><?php echo $titulo;?></h1>
-                  </div>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</head>
+
+<body>
+  <div class="login-page">
+    <div class="container d-flex align-items-center">
+      <div class="form-holder has-shadow">
+        <div class="row">
+          <!-- Logo & Information Panel-->
+          <div class="col-lg-6">
+            <div class="info d-flex align-items-center">
+              <div class="content">
+                <div class="logo">
+                  <h1><?php echo $titulo; ?></h1>
                 </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
               </div>
             </div>
-            <!-- Form Panel    -->
-            <div class="col-lg-6 bg-white">
-              <div class="form d-flex align-items-center">
-                <div class="content">
+          </div>
+          <!-- Form Panel    -->
+          <div class="col-lg-6 bg-white">
+            <div class="form d-flex align-items-center">
+              <div class="content">
 
                 <?php echo form_open('/', ['id' => 'form', 'class' => 'form-validate']); ?>
 
-                    <div class="form-group">
-                      <input id="login-username" type="email" name="email" required data-msg="Por favor informe seu e-mail" class="input-material">
-                      <label for="login-username" class="label-material">Seu e-mail de acesso</label>
-                    </div>
-                    <div class="form-group">
-                      <input id="login-password" type="password" name="password" required data-msg="Por favor informe sua senha" class="input-material">
-                      <label for="login-password" class="label-material">Sua senha</label>
-                    </div>
-                    <input id="btn-login" type="submit" class="btn btn-primary" value="Entrar">
-                
-                <?php echo form_close(); ?>             
-                  <a href="#" class="forgot-pass mt-3">Esqueceu a sua senha?</a>
+                <div id="response">
+
                 </div>
+
+                <div class="form-group">
+                  <input id="login-username" type="email" name="email" required data-msg="Por favor informe seu e-mail" class="input-material">
+                  <label for="login-username" class="label-material">Seu e-mail de acesso</label>
+                </div>
+                <div class="form-group">
+                  <input id="login-password" type="password" name="password" required data-msg="Por favor informe sua senha" class="input-material">
+                  <label for="login-password" class="label-material">Sua senha</label>
+                </div>
+                <input id="btn-login" type="submit" class="btn btn-primary" value="Entrar">
+
+                <?php echo form_close(); ?>
+                <a href="#" class="forgot-pass mt-3">Esqueceu a sua senha?</a>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="copyrights text-center">
-         <p>2018 &copy; Your company. Download From <a target="_blank" href="https://templateshub.net">Templates Hub</a>.</p>
-      </div>
     </div>
-    <!-- JavaScript files-->
-    <script src="<?php echo site_url('recursos/') ?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?php echo site_url('recursos/') ?>vendor/popper.js/umd/popper.min.js"> </script>
-    <script src="<?php echo site_url('recursos/') ?>vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?php echo site_url('recursos/') ?>vendor/jquery-validation/jquery.validate.min.js"></script>
-    <script src="<?php echo site_url('recursos/') ?>js/front.js"></script>
-    
-  </body>
+    <div class="copyrights text-center">
+      <p>2018 &copy; Your company. Download From <a target="_blank" href="https://templateshub.net">Templates Hub</a>.</p>
+    </div>
+  </div>
+  <!-- JavaScript files-->
+  <script src="<?php echo site_url('recursos/') ?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo site_url('recursos/') ?>vendor/popper.js/umd/popper.min.js"> </script>
+  <script src="<?php echo site_url('recursos/') ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="<?php echo site_url('recursos/') ?>vendor/jquery-validation/jquery.validate.min.js"></script>
+  <script src="<?php echo site_url('recursos/') ?>js/front.js"></script>
+  <script>
+    $(document).ready(function() {
+      $("#form").on('submit', function(e) {
+        e.preventDefault();
+
+        $.ajax({
+          type: 'POST',
+          url: '<?php echo site_url('login/logar'); ?>',
+          data: new FormData(this),
+          dataType: 'json',
+          contentType: false,
+          cache: false,
+          processData: false,
+          beforeSend: function() {
+            $("#response").html('');
+            $("#btn-login").val('Por favor aguarde...');
+          },
+          success: function(response) {
+            $("#btn-login").val('Salvar');
+            $("#btn-login").removeAttr("disabled");
+            $('[name=csrf_ordem]').val(response.token);
+
+            if (!response.erro) {
+              //tudo certo na atualização do usuario.
+              window.location.href = "<?php echo site_url() ?>" + response.redirect;
+            }
+
+            if (response.erro) {
+              $("#response").html('<div class="alert alert-danger">' + response.erro + '</div>');
+
+              if (response.erros_model) {
+                $.each(response.erros_model, function(key, value) {
+                  $("#response").append('<ul class="list-unstyled"><li class="text-danger">' + value + '</li></ul>')
+                });
+              }
+            }
+          },
+          error: function() {
+            alert('Não foi possivel processar a solicitação. Por favor entre em contato com o suporte técnico.');
+            $("#btn-login").val('Salvar');
+            $("#btn-login").removeAttr("disabled");
+          }
+        });
+      })
+
+      $("#form").submit(function() {
+        $(this).find(":submit").attr('disabled', 'disabled');
+      })
+    });
+  </script>
+
+</body>
+
 </html>
