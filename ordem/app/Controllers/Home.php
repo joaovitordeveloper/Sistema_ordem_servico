779ea5d2be1f167c1a9a7e1e4b32ef90705f0c73
Joao Vitor <developer.joaovitor@gmail.com>
2023-04-11 20:09:04 -0300

    mandando email

--- ordem/app/Controllers/Home.php
+++ ordem/app/Controllers/Home.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Libraries\Autenticacao;
 
 class Home extends BaseController
@@ -11,17 +12,36 @@ class Home extends BaseController
         $data = [
             'titulo' => 'Home'
         ];
-        
+
         return view('Home/index', $data);
     }
 
     public function login()
     {
         $autenticacao = service('autenticacao');
-        
-        $autenticacao->login('teste2@teste.com','12345678');
+
+        $autenticacao->login('teste2@teste.com', '12345678');
         $usuario = $autenticacao->pegaUsuarioLogado();
 
         dd($usuario);
+    }
+
+    public function email()
+    {
+        $email = service('email');
+
+        $email->setFrom('teste@allinsystems.com.br', 'Ordem de serviço LTDA');//de onde vai sair o e-mail
+        $email->setTo('developer.joaovitor@gmail.com');//pra quem vai o e-mail
+
+        $email->setSubject('Recuperação de senha');
+        $email->setMessage('Iniciando a recuperação de senha');
+
+        if ($email->send()) {
+            echo "Email enviado";
+        }else{
+            echo $email->printDebugger();
+        }
+
+        
     }
 }
